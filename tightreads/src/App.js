45b653b3c26af4c33f35f055c70efb096057746b
Sunny Phan <sphan010@ucr.edu>
2019-10-20 19:34:07 -0700

    Installed react-router-dom depend. and set up basic routes

--- tightreads/src/App.js
+++ tightreads/src/App.js
@@ -2,24 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+//Pages
+import LoginPage from "./pages";
+import NotFoundPage from "./pages/404";
+import MakeAccountsPage from "./pages/makeaccounts";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path="/" component={LoginPage}/>
+      <Route exact path="/makeaccounts" component={MakeAccountsPage}/>
+      <Route exact path="/404" component={NotFoundPage}/>  {/*} Redirect to 404 Page at end of component if no other component found */}
+      <Redirect to="/404"/>
+      </Switch>
+    </Router>
   );
 }
 
