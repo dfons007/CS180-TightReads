a5c6f0c2101033390f16fc9891b8e3eb98eada1e
nailcliper <ltayanami@hotmail.com>
2019-11-09 15:02:42 -0800

    Implement backend for displaying book profile
    Using Google Books API. Current book entry is hardcoded. Some book entries have missing JSON elements and lead to compile errors

--- tightreads/src/pages/book_UI.jsx
+++ tightreads/src/pages/book_UI.jsx
@@ -2,65 +2,89 @@ import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
 
+import { getGoogleBook } from '../google.js';
+
 class Book_UI extends Component {
 	constructor () {
 		super();
 		this.state = {   
+            id: "",
+            
 			title: 'BOOK TITLE',
+            subtitle: '',
 			author: 'BOOK AUTHOR',
 			genre: 'BOOK GENRE',
-			summary: 'This is the book sumary... blah blah blah...'
+			summary: 'This is the book sumary... blah blah blah...',
+            artwork: "https://via.placeholder.com/500",
 
 		};
 	}
 
 	render() {
-		 return (
-    <MDBCard className="my-5 px-5 pb-5">
-      <MDBCardBody>
-        <MDBRow>
-          <MDBCol lg="5">
-            <MDBView >
-              <img src="https://via.placeholder.com/500" />
-            </MDBView>
-          </MDBCol>
-          <MDBCol lg="7">
-            <a href="#!" className="green-text">
-              <h6 className="font-weight-bold mb-3">
-                <MDBIcon icon="book" className="pr-2" />
-                Books
-              </h6>
-            </a>
-            <h1>
-              <strong>{this.state.title} </strong>
-            </h1>
-            <h5>
-            	<b>Author: </b> 
-            	<a href="ENTER HYPERLINK HERE TO AUTHOR">
-                	{this.state.author} 
-              	</a>
-            </h5>
+        return (
+            <MDBCard className="my-5 px-5 pb-5">
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol lg="5">
+                    <MDBView >
+                      <img src={this.state.artwork} />
+                    </MDBView>
+                  </MDBCol>
+                  <MDBCol lg="7">
+                    <a href="#!" className="green-text">
+                      <h6 className="font-weight-bold mb-3">
+                        <MDBIcon icon="book" className="pr-2" />
+                        Books
+                      </h6>
+                    </a>
+                    <h1>
+                      <strong>{this.state.title} </strong>
+                      <br/>{this.state.subtitle}
+                    </h1>
+                    <h5>
+                        <b>Author: </b> 
+                        <a href="ENTER HYPERLINK HERE TO AUTHOR">
+                            {this.state.author} 
+                        </a>
+                    </h5>
 
-            <h5>
-            	<b>Genre: </b> {this.state.genre} 
-            </h5>
-            <h5>
-            	<b>Summary:</b> 
-            </h5>
-            <p>
-            	{this.state.summary} 
-            </p>
-            <MDBBtn color="success" size="md">
-            	button
-            </MDBBtn>
-          </MDBCol>
-        </MDBRow>
-      </MDBCardBody>
-    </MDBCard>
-  );
+                    <h5>
+                        <b>Genre: </b> {this.state.genre} 
+                    </h5>
+                    <h5>
+                        <b>Summary:</b> 
+                    </h5>
+                    <p>
+                        {this.state.summary} 
+                    </p>
+                    <MDBBtn color="success" size="md">
+                        button
+                    </MDBBtn>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+        );
 	}
+    
+    componentDidMount() {
+        const id = "HestSXO362YC";//this.props.match.params;
+        this.setState({
+            id: id,
+        });
+        getGoogleBook(id).then(data => {
+            console.log(data)
+            this.setState({
+                title: (data.volumeInfo.title) ? data.volumeInfo.title : '',
+                subtitle: (data.volumeInfo.subtitle) ? data.volumeInfo.subtitle : '',
+                author: (data.volumeInfo.authors) ? data.volumeInfo.authors[0] : '',
+                genre: (data.volumeInfo.categories) ? data.volumeInfo.categories[0]  : '',
+                summary: (data.volumeInfo.description) ? data.volumeInfo.description : '',
+                artwork: (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.thumbnail : '',
+            });
+        });
+    }
 }
-
 
 export default Book_UI;
 
