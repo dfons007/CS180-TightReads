65ee06ef6a885024279685098327b8415d30614c
Sunny Phan <sphan010@ucr.edu>
2019-11-16 17:39:23 -0800

    Add navbar to bookprofile page

--- tightreads/src/pages/book_UI.jsx
+++ tightreads/src/pages/book_UI.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {Link, Redirect} from "react-router-dom";
-import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
-
+import {MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn } from "mdbreact";
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 import { getGoogleBook, getGoogleSearch } from '../google.js';
 
 class Book_UI extends Component {
@@ -22,34 +23,65 @@ class Book_UI extends Component {
 
 	render() {
 		return (
-			<div class="container">
 
-				<div class="row">
-					<div class="col-sm-4">
-						<img src={this.state.artwork} alt='book image' width={350} height={500} mode='fit'/>
-					</div>
+			<>
 
-					<div class="col-sm-8">
-						<h1>
-							<strong>{this.state.title} </strong>
-							<br/>{this.state.subtitle}
-						</h1>
-						<h5>
-							<b>Author: </b> {this.state.author} 
-						</h5>
-						<h5>
-							<b>Genre: </b> {this.state.genre} 
-						</h5>
-						<h5>
-							<b>Summary:</b> 
-						</h5>
-						<p>
-							{this.state.summary} 
-						</p>
-						
+
+				<Navbar bg="dark" variant="dark">
+				<LinkContainer to="/homepage">
+				<Navbar.Brand href="#home">TightReads</Navbar.Brand>
+				</LinkContainer>
+				<Nav className="mr-auto">
+					<LinkContainer to="/homepage">
+					<Nav.Link>Home</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/">
+					<Nav.Link>Login</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to="/makeaccounts">
+					<Nav.Link>Sign Up</Nav.Link>
+					</LinkContainer>            
+				</Nav>
+				<Form inline>
+					<FormControl type="text" placeholder="Search Books" className="mr-sm-2" />
+					<Button variant="outline-light">Search</Button>
+				</Form>
+				</Navbar>
+
+				<br/>
+
+				<div class="container">
+
+
+
+
+					<div class="row">
+						<div class="col-sm-4">
+							<img src={this.state.artwork} alt='book image' width={350} height={500} mode='fit'/>
+						</div>
+
+						<div class="col-sm-8">
+							<h1>
+								<strong>{this.state.title} </strong>
+								<br/>{this.state.subtitle}
+							</h1>
+							<h5>
+								<b>Author: </b> {this.state.author} 
+							</h5>
+							<h5>
+								<b>Genre: </b> {this.state.genre} 
+							</h5>
+							<h5>
+								<b>Summary:</b> 
+							</h5>
+							<p>
+								{this.state.summary} 
+							</p>
+							
+						</div>
 					</div>
 				</div>
-			</div>
+			</>
 		);
 	}
     
